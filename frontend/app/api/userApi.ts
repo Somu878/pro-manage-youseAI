adbfdd94250b8d1c3d4cf58c894147a1c6a58e97
Somu878 <somukandula99@gmail.com>
2024-09-29 11:33:43 +0530

    #fix edge function error middleware

--- frontend/app/api/userApi.ts
+++ frontend/app/api/userApi.ts
@@ -1,28 +1,30 @@
-import {setCookie} from "cookies-next"
+import { setCookie } from "cookies-next";
 import axiosInstance from "./axiosClient";
-import { userSigninSchema, userSignupSchema } from "@/lib/validations/user-validation";
+import {
+  userSigninSchema,
+  userSignupSchema,
+} from "@/lib/validations/user-validation";
 
-export async function SignInUser(user: typeof userSigninSchema){
-    const response = await axiosInstance.post(`/user/signin`, user);
-    setCookie("token", response.data.token as unknown as string);
-    return response;
+export async function SignInUser(user: typeof userSigninSchema) {
+  const response = await axiosInstance.post(`/user/signin`, user);
+  setCookie("token", response.data.token as unknown as string);
+  return response;
 }
 
-export async function SignUpUser(user: typeof userSignupSchema){
-    const response = await axiosInstance.post(`/user/signup`, user);
-    setCookie("token", response.data.token as unknown as string);
-    return response;
+export async function SignUpUser(user: typeof userSignupSchema) {
+  const response = await axiosInstance.post(`/user/signup`, user);
+  setCookie("token", response.data.token as unknown as string);
+  return response;
 }
 
-export async function GetUser(token: string){
-   
+export async function GetUser(token: string) {
   try {
     const response = await axiosInstance.get(`/user/profile`, {
-        headers: {
-            authorization: token
-        }
+      headers: {
+        authorization: token,
+      },
     });
-    return response.data
+    return response.data;
   } catch (error) {
     console.log(error);
     return null;
--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,31 +1,31 @@
-"use server"
-import { NextRequest, NextResponse } from 'next/server';
-import { GetUser } from './app/api/userApi';
-import { cookies } from 'next/headers';
+"use server";
+import { NextRequest, NextResponse } from "next/server";
+import { GetUser } from "./app/api/userApi";
+import { cookies } from "next/headers";
 
 async function middleware(req: NextRequest, res: NextResponse) {
-    // if (!cookies().get("token")){
-    //     return NextResponse.redirect(new URL('/auth', req.url))
-    // }
-    try {
-        const token = cookies().get("token")?.value;
-        const response = await GetUser(token as string);
+  // if (!cookies().get("token")){
+  //     return NextResponse.redirect(new URL('/auth', req.url))
+  // }
+  try {
+    const token = cookies().get("token")?.value;
+    const response = await GetUser(token as string);
 
-        if (!response) {
-            return NextResponse.redirect(new URL('/on-board', req.url));
-        } else {
-            return NextResponse.next();
-        }
-    } catch (error) {
-        console.error("Error fetching user:", error);
-        return NextResponse.redirect(new URL('/on-board', req.url));
+    if (!response?.user) {
+      return NextResponse.redirect(new URL("/on-board", req.url));
+    } else {
+      return NextResponse.next();
     }
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    return NextResponse.redirect(new URL("/on-board", req.url));
+  }
 }
 
 export default middleware;
 
 export const config = {
-    matcher: ["/dashboard"],
-    // Exclude the authentication route from the middleware
-    ignoreRoutes: ["/on-board"],
+  matcher: ["/dashboard"],
+  // Exclude the authentication route from the middleware
+  ignoreRoutes: ["/on-board"],
 };

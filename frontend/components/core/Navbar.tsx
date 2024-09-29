f8adb5640944c06fabf378f6103455630ce3f15c
Somu878 <somukandula99@gmail.com>
2024-09-29 12:24:40 +0530

    update darkmode styles

--- frontend/components/core/Navbar.tsx
+++ frontend/components/core/Navbar.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
-import { Button } from "@/components/ui/button"
+import React from "react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { deleteCookie} from "cookies-next";
+import { deleteCookie } from "cookies-next";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,12 +10,12 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Loader2 } from 'lucide-react';
-import { ThemeToggle } from './ThemeToggle';
+} from "@/components/ui/dropdown-menu";
+import { Loader2 } from "lucide-react";
+import { ThemeToggle } from "./ThemeToggle";
 
 interface NavbarProps {
-  userName: User
+  userName: User;
 }
 
 export interface User {
@@ -24,49 +24,55 @@ export interface User {
 }
 
 export default function Navbar({ userName }: NavbarProps) {
-    const router = useRouter();
+  const router = useRouter();
   const handleLogout = () => {
-    deleteCookie("token")
-    router.push("/on-board")
-  }
- 
+    deleteCookie("token");
+    router.push("/on-board");
+  };
   return (
-    <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="bg-white border-b border-gray-200 px-4 py-2.5 dark:bg-[#262626] dark:border-gray-700">
       <div className="flex flex-wrap justify-between items-center">
         <div className="flex items-center">
-          <span className="self-center text-m font-semibold whitespace-nowrap md:xl dark:text-white">Pro Manage</span>
+          <span className="self-center text-m font-semibold whitespace-nowrap md:text-xl dark:text-white">
+            Pro Manage
+          </span>
         </div>
         <div className="flex items-center">
-        <div className="mr-4">  <ThemeToggle /></div>
-          <span className="hidden md:block mr-4 text-sm font-medium text-gray-900 dark:text-white">Welcome, {userName?.name as unknown as string}</span>
+          <div className="mr-4">
+            {" "}
+            <ThemeToggle />
+          </div>
+          <span className="hidden md:block mr-4 text-sm font-medium text-gray-900 dark:text-white">
+            Welcome, {userName?.name as unknown as string}
+          </span>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
               <Button variant="ghost" className="relative h-8 w-8 rounded-full">
                 <Avatar className="h-8 w-8">
                   {/* <AvatarImage src="/placeholder-avatar.jpg" alt={userName} /> */}
 
-                  <AvatarFallback>{userName?.name.charAt(0) as unknown as string}</AvatarFallback>
+                  <AvatarFallback className="dark:bg-[#000000]">
+                    {userName?.name.charAt(0) as unknown as string}
+                  </AvatarFallback>
                 </Avatar>
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent className="w-56" align="end" forceMount>
               <DropdownMenuLabel className="font-normal">
                 <div className="flex flex-col space-y-1">
-                  <p className="text-sm font-medium leading-none">{userName?.name as unknown as string}</p>
+                  <p className="text-sm font-medium leading-none">
+                    {userName?.name as unknown as string}
+                  </p>
                   <p className="text-xs leading-none text-muted-foreground">
                     {userName?.email as unknown as string}
                   </p>
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                Profile
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                Settings
-              </DropdownMenuItem>
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
               <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={handleLogout} className='text-red-500'>
+              <DropdownMenuItem onClick={handleLogout} className="text-red-500">
                 Log out
               </DropdownMenuItem>
             </DropdownMenuContent>
@@ -74,5 +80,5 @@ export default function Navbar({ userName }: NavbarProps) {
         </div>
       </div>
     </nav>
-  )
+  );
 }
--- frontend/components/core/board/board-view.tsx
+++ frontend/components/core/board/board-view.tsx
@@ -1,15 +1,14 @@
 import { useEffect, useState } from "react";
 import Task from "./Task";
-import {  TaskStatus, Task as TaskType } from "@/lib/types";
+import { TaskStatus, Task as TaskType } from "@/lib/types";
 import { Draggable, Droppable, DropResult } from "react-beautiful-dnd";
 import { DndContext } from "@/components/context/DndContext";
 import { useBoard } from "@/app/hooks/UseBoard";
 import BoardLoader from "@/components/ui/BoardLoader";
 import { Loader2 } from "lucide-react";
 
-
 function BoardView() {
-  const{board,updateBoard,isLoading,error,refreshBoard}=useBoard()
+  const { board, updateBoard, isLoading, error, refreshBoard } = useBoard();
   const [tasks, setTasks] = useState<Record<TaskStatus, TaskType[]>>({
     todo: [],
     in_progress: [],
@@ -18,21 +17,21 @@ function BoardView() {
 
   useEffect(() => {
     setTasks({
-      todo: board.filter((task) => task.status === 'todo'),
-      in_progress: board.filter((task) => task.status === 'in_progress'),
-      done: board.filter((task) => task.status === 'done'),
+      todo: board.filter((task) => task.status === "todo"),
+      in_progress: board.filter((task) => task.status === "in_progress"),
+      done: board.filter((task) => task.status === "done"),
     });
   }, [board]);
-  const columnsOrder = ['todo', 'in_progress', 'done'];
+  const columnsOrder = ["todo", "in_progress", "done"];
   const columnTitles = {
-    todo: 'To Do',
-    in_progress: 'In Progress',
-    done: 'Done',
+    todo: "To Do",
+    in_progress: "In Progress",
+    done: "Done",
   };
 
   const onDragEnd = async (result: DropResult) => {
-    const { source, destination} = result;
-   if (!destination) return;
+    const { source, destination } = result;
+    if (!destination) return;
     if (source.droppableId === destination.droppableId) {
       const column = source.droppableId as keyof typeof tasks;
       const reorderedTasks = Array.from(tasks[column]);
@@ -42,7 +41,7 @@ function BoardView() {
         ...prev,
         [column]: reorderedTasks,
       }));
-      console.log(tasks)
+      console.log(tasks);
     } else {
       // Moving task between columns
       const sourceColumn = source.droppableId as keyof typeof tasks;
@@ -50,11 +49,11 @@ function BoardView() {
       const sourceTasks = Array.from(tasks[sourceColumn]);
       const destTasks = Array.from(tasks[destinationColumn]);
       // Remove the task from the source column
-      const [movedTask]= sourceTasks.splice(source.index, 1) as TaskType[]
-      console.log(movedTask)
+      const [movedTask] = sourceTasks.splice(source.index, 1) as TaskType[];
+      console.log(movedTask);
       // Add the task to the destination column
       destTasks.splice(destination.index, 0, movedTask);
-      console.log(destTasks)
+      console.log(destTasks);
       //update the task's status when moving between columns
       movedTask.status = destinationColumn as TaskStatus;
       // Update the state with tasks in both columns
@@ -63,40 +62,51 @@ function BoardView() {
         [sourceColumn]: sourceTasks,
         [destinationColumn]: destTasks,
       }));
-      const all = [
-        ...tasks.todo,  
-        ...tasks.in_progress,
-        ...tasks.done
-      ]
-    updateBoard(all)
+      const all = [...tasks.todo, ...tasks.in_progress, ...tasks.done];
+      updateBoard(all);
     }
   };
-if(tasks.todo.length===0 && tasks.in_progress.length===0 && tasks.done.length===0) return <div className="text-center text-gray-500 flex items-center justify-center h-[550px]" >No tasks found <br /> Add some tasks to get started</div>
-if(isLoading) return <div className="text-center text-gray-500 flex items-center justify-center h-[550px]" >
- <Loader2 className='w-7   h-7 animate-spin' />
-</div>
+  if (
+    tasks.todo.length === 0 &&
+    tasks.in_progress.length === 0 &&
+    tasks.done.length === 0
+  )
+    return (
+      <div className="text-center text-gray-500 flex items-center justify-center h-[550px]">
+        No tasks found <br /> Add some tasks to get started
+      </div>
+    );
+  if (isLoading)
+    return (
+      <div className="text-center text-gray-500 flex items-center justify-center h-[550px]">
+        <Loader2 className="w-7   h-7 animate-spin" />
+      </div>
+    );
 
-if (error) return <div>Error: {error}</div>;
+  if (error) return <div>Error: {error}</div>;
   return (
     <DndContext onDragEnd={onDragEnd}>
       <div className="p-4">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          {columnsOrder.map((column,index) => (
-            <Droppable key={index} droppableId={column} isCombineEnabled={true} >
+          {columnsOrder.map((column, index) => (
+            <Droppable key={index} droppableId={column} isCombineEnabled={true}>
               {(provided) => (
                 <div
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-                className="bg-gray-100 rounded-lg shadow-md p-4 min-h-[550px]
-                dark:bg-gray-900 dark:text-white"
-              >
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="bg-gray-100 rounded-lg shadow-md p-4 min-h-[550px]
+                dark:bg-[#262626] dark:text-white"
+                >
                   <h2 className="text-lg font-bold mb-2">
                     {columnTitles[column as keyof typeof columnTitles]}
                   </h2>
                   <div className="flex flex-col gap-2">
                     {tasks[column as keyof typeof tasks].map((task, index) => (
-                    
-                      <Draggable key={task.id} draggableId={task.id.toString()} index={index}>
+                      <Draggable
+                        key={task.id}
+                        draggableId={task.id.toString()}
+                        index={index}
+                      >
                         {(provided) => (
                           <div
                             key={task.id}
@@ -107,7 +117,10 @@ if (error) return <div>Error: {error}</div>;
                             rounded-md
                             active:shadow-lg active:shadow-gray-400"
                           >
-                         <Task task={task} refreshBoard={()=>refreshBoard()} />
+                            <Task
+                              task={task}
+                              refreshBoard={() => refreshBoard()}
+                            />
                           </div>
                         )}
                       </Draggable>
